648bcf5a31fef8b2f89677b2d9251c1ddd85b42f
marrrxx <144335737+marrrxx@users.noreply.github.com>
2023-11-09 23:19:00 +0000

    we hebben de stof goed doorgenomen

--- script.js
+++ script.js
@@ -1,42 +1,171 @@
-var x = 50;
-var y = 50;
+
+class Raster {
+  constructor(r,k) {
+    this.aantalRijen = 12;
+    this.aantalKolommen = 18;
+    this.celGrootte = null;
+    }
+
+
+berekenCelGrootte() {
+    this.celGrootte = canvas.width / this.aantalKolommen;
+  }
+
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        if (rij === 15  || kolom === 17 ) {
+           fill ("orange");
+         } else
+         noFill();
+              rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
+
+class Bom {
+  constructor() {
+    this.x = floor(random(1,raster.aantalKolommen))*raster.celGrootte;
+    this.y = floor(random(0,raster.aantalRijen))*raster.celGrootte;
+    this.speed = random 
+  }
+    toon() {
+      image(bomPlaatje,this.x,this.y,raster.celGrootte,raster.celGrootte));
+    }
+  }
+
+
+
+
+
+class Jos {
+  constructor() {
+    this.x = 400;
+    this.y = 300;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null; 
+    this.gehaald = false;
+  }
+
+
+    beweeg() {
+    if (keyIsDown(65)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(68)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(87)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(83)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+    
+  this.x = constrain(this.x,0,canvas.width);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+
+    if (this.x == canvas.width) {
+      this.gehaald = true;
+    }
+  }
+  
+wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}  
+  
+
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  bomPlaatje = loadImage("images/sprites/bom_100px.png");
+
+}
 
 function setup() {
-  canvas = createCanvas(1000,400);
+  canvas = createCanvas(900,600);
   canvas.parent();
+  frameRate(10);
   textFont("Verdana");
-  textSize(14);
-  noStroke();
-  frameRate(50);
-}
+  textSize(90);
+  
+  raster = new Raster(12,18);
+  
+  raster.berekenCelGrootte();
+  
+  eve = new Jos();
+  eve.stapGrootte = 1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
+  }
+  
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
 
-function draw() {
-  background('olive');
-  
-  if (keyIsDbown(LEFT_ARROW)) {
-    x -= 5;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    x += 5;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    y -= 5;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    y += 5;
-  }
-  x = constrain(x,0,width - 100);
-  y = constrain(y,0,height - 100);
-
-  if (x >= 700 && x <= 875 && y >= 75 && y <= 225) {
-    fill('chartreuse');
-  }
-  else {
-    fill('darkkhaki');
-  }
-  
-  rect(800,175,75,50);
-  
-  fill('moccasin');
-  rect(x,y,100,100);   
+  bob = new Vijand(600,400);
+  bob.stapGrootte = 1*eve.stapGrootte;
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png"); 
 }
+  function draw() {
+    background(brug);
+    raster.teken();
+    eve.beweeg();
+    alice.beweeg();
+    bob.beweeg();
+    eve.toon();
+    alice.toon();
+    bob.toon();
+
+    if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
+      noLoop();
+    }
+
+    if (eve.gehaald) {
+      background('green');
+      fill('white');
+      text("Je hebt gewonnen!",30,300);
+      noLoop();
+    }
+  }
